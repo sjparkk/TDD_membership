7b2f907a99ac9a00d43ef41e4929115c18cae8a1
sjparkk <sjparkk0820@naver.com>
2021-12-12 14:13:37 +0900

    feat : 유저 아이디와 멤버쉽타입으로 조회 기능 테스트 추가

--- src/main/java/com/tdd/membership/app/repositoyry/MembershipRepository.java
+++ src/main/java/com/tdd/membership/app/repositoyry/MembershipRepository.java
@@ -1,7 +1,9 @@
 package com.tdd.membership.app.repositoyry;
 
 import com.tdd.membership.app.entity.Membership;
+import com.tdd.membership.app.enums.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Membership findByUserIdAndMembershipType(String id, MembershipType membershipType);
 }
--- src/test/java/com/tdd/membership/app/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/tdd/membership/app/membership/repository/MembershipRepositoryTest.java
@@ -3,7 +3,6 @@ package com.tdd.membership.app.membership.repository;
 import com.tdd.membership.app.entity.Membership;
 import com.tdd.membership.app.enums.MembershipType;
 import com.tdd.membership.app.repositoyry.MembershipRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.*;
 
+//@Transactional 포함하고 있어 자동 롤백
 @DataJpaTest
 public class MembershipRepositoryTest {
 
@@ -41,5 +41,26 @@ public class MembershipRepositoryTest {
         assertThat(result.getUserId()).isEqualTo("id");
         assertThat(result.getMembershipType()).isEqualTo(MembershipType.NAVER);
         assertThat(result.getPoint()).isEqualTo(10000);
+    }
+
+    @Test
+    @DisplayName("아이디와 멤버쉽타입으로 해당 멤버쉽을 조회할 수 있는지 테스트")
+    public void read() {
+        //given
+        final Membership membership = Membership.builder()
+                .userId("id")
+                .membershipType(MembershipType.NAVER)
+                .point(10000)
+                .build();
+        //when
+        membershipRepository.save(membership);
+        Membership resultMembership = membershipRepository.findByUserIdAndMembershipType("id", MembershipType.NAVER);
+        //then
+        assertThat(resultMembership).isNotNull(); //객체 널체크
+        assertThat(resultMembership.getId()).isNotNull(); // 아이디로 널체크
+        assertThat(resultMembership.getUserId()).isEqualTo("id"); // userid 값 체크
+        assertThat(resultMembership.getMembershipType()).isEqualTo(MembershipType.NAVER); // type체크
+        assertThat(resultMembership.getPoint()).isEqualTo(10000); // point 값 체크
+
     }
 }
