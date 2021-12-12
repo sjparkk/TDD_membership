ca07c39e2b2f2b02d84ce23430ec9608fe019acd
sjparkk <sjparkk0820@naver.com>
2021-12-12 13:02:42 +0900

    feat : 멤버쉽 등록 테스트 추가 및 엔티티 추가 어노테이션 설정

--- src/test/java/com/tdd/membership/app/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/tdd/membership/app/membership/repository/MembershipRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.tdd.membership.app.membership.repository;
 
+import com.tdd.membership.app.entity.Membership;
 import com.tdd.membership.app.repositoyry.MembershipRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 public class MembershipRepositoryTest {
@@ -16,6 +19,26 @@ public class MembershipRepositoryTest {
     @Test
     @DisplayName("멤버쉽리포지토리 널체크")
     public void MembershipRepositoryIsNotNull() {
-        Assertions.assertThat(membershipRepository).isNotNull();
+        assertThat(membershipRepository).isNotNull();
+    }
+
+    @Test
+    @DisplayName("멤버쉽 등록")
+    public void create() {
+        //given
+        final Membership membership = Membership.builder()
+                .userId("id")
+                .membershipName("네이버")
+                .point(10000)
+                .build();
+
+        //when
+        final Membership result = membershipRepository.save(membership);
+
+        //then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getUserId()).isEqualTo("id");
+        assertThat(result.getMembershipName()).isEqualTo("네이버");
+        assertThat(result.getPoint()).isEqualTo(10000);
     }
 }
