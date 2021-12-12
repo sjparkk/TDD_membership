fdcf798b58477041767c2a5c499dd4930fb85157
sjparkk <sjparkk0820@naver.com>
2021-12-12 12:51:59 +0900

    feat : 멤버쉽 엔티티 설정

--- src/main/java/com/tdd/membership/app/entity/Membership.java
+++ src/main/java/com/tdd/membership/app/entity/Membership.java
@@ -1,4 +1,15 @@
 package com.tdd.membership.app.entity;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@NoArgsConstructor
 public class Membership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
--- src/test/java/com/tdd/membership/app/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/tdd/membership/app/membership/repository/MembershipRepositoryTest.java
@@ -5,7 +5,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 public class MembershipRepositoryTest {
 
     @Autowired
