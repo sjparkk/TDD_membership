9fa843fcc01f640589b9747d4cf03df0b5387434
sjparkk <sjparkk0820@naver.com>
2021-12-12 12:07:33 +0900

    feat 멤버쉽 리포지토리 테스트 생성

---
+++ src/test/java/com/tdd/membership/app/membership/repository/MembershipRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.tdd.membership.app.membership.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MembershipRepositoryTest {
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+}
